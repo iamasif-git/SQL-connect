e95afa9267fc5aa4ac3445a165bdfc6da5b69ee2
Md Asif <mdasif.uem@gmail.com>
2021-09-16 00:49:27 +0000

    Added Functions for connecting to MYsql, cursor and closing connection

---
+++ mysql-connect.py
@@ -0,0 +1,27 @@
+import mysql.connector
+import Error
+
+def mysql_connect(hostname,username,password,db):
+    try:
+        connection = mysql.connector.connect(
+        host = hostname,
+        user = username,
+        passwd = password,
+        database = db
+        )
+        print("Successfully connected")
+    except Error as e:
+        print(e)
+    return connection
+
+def cursor_connect(connection,):
+    cursor = connection.cursor()
+
+def close_connection(cursor,connection):
+    try:
+        cursor.close()
+        connection.close()
+        print("MYSQL connection closed")
+    except Error as e:
+        print(e)
+                
